bb6332589480151df5dd4d0571d946ff0948ffb9
cc3728 <cc3728@drexel.edu>
2020-03-30 21:15:47 -0400

    Created individual function STLWriter in MazeLib

---
+++ MazeLib/STLWriter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace MazeLib
+{
+    class STLWriter
+    {
+        public void STLExport()
+        {
+            
+            //List<Triangle> mesh = new List<Triangle>();
+            //get mzPoint1
+            //mzPoint1node
+            //get mzPoint2
+            //get mzPoint3
+            //get mzPoint4
+        }
+
+        public struct Vertex
+        {
+            public float X;
+            public float Y;
+            public float Z;
+            public Vertex(float Xin, float Yin, float Zin)
+            {
+
+            }
+        }
+
+    }
+}
